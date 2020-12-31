db6cf38afc143cb37241435e92d2fea7008ba8ce
Greg D <gdivis@inedo.com>
2020-12-30 23:39:48 -0500

    fixed PSVerify to also work like PSCall

--- Scripting/InedoExtension/Operations/PowerShell/PSVerifyOperation.cs
+++ Scripting/InedoExtension/Operations/PowerShell/PSVerifyOperation.cs
@@ -1,16 +1,15 @@
 ﻿using System.Collections.Generic;
 using System.ComponentModel;
-using System.Threading;
 using System.Threading.Tasks;
-using Inedo.Agents;
 using Inedo.Diagnostics;
 using Inedo.Documentation;
 using Inedo.ExecutionEngine;
+using Inedo.ExecutionEngine.Mapping;
 using Inedo.Extensibility;
 using Inedo.Extensibility.Configurations;
 using Inedo.Extensibility.Operations;
 using Inedo.Extensions.Scripting.PowerShell;
-using Inedo.Web;
+using Inedo.Web.Editors.Operations;
 
 namespace Inedo.Extensions.Scripting.Operations.PowerShell
 {
@@ -18,139 +17,98 @@ namespace Inedo.Extensions.Scripting.Operations.PowerShell
     [Description("Uses a PowerShell script to collect configuration about a server.")]
     [ScriptAlias("PSVerify")]
     [Tag(Tags.PowerShell)]
+    [UsesCallScriptEditor(typeof(PowerShellScriptInfoProvider))]
     [ScriptNamespace(Namespaces.PowerShell, PreferUnqualified = true)]
-    [Note("The Key is a unique string per server, and having multiple operations attempt to use the same key will yield in unpredictable behavior.")]
-    public sealed class PSVerifyOperation : VerifyOperation
+    public sealed class PSVerifyOperation : VerifyOperation, ICustomArgumentMapper
     {
-        private PSProgressEventArgs currentProgress;
+        private volatile PSProgressEventArgs currentProgress;
 
-        [Required]
-        [ScriptAlias("Key")]
-        [DisplayName("Configuration key")]
-        public string ConfigurationKey { get; set; }
-        [Required]
-        [ScriptAlias("Value")]
-        [DisplayName("Expected value")]
-        public string ExpectedValue { get; set; }
-        [ScriptAlias("Collect")]
-        [DisplayName("Collection script")]
-        [Description("The output of this PowerShell script will be used to collect the current configuration of the server.")]
-        [Category("Collect")]
-        [DisableVariableExpansion]
-        public string CollectScript { get; set; }
-        [ScriptAlias("CollectScript")]
-        [DisplayName("Collection script asset")]
-        [Description("The name of a PowerShell script asset to use for collection. The output of this PowerShell script will be used to collect the current configuration of the server.")]
-        [Category("Collect")]
-        public string CollectScriptAsset { get; set; }
-        [ScriptAlias("UseExitCode")]
-        [DisplayName("Use exit code")]
-        [Description("When set, the exit/return code of the script will be used instead of the output stream for collection.")]
-        [Category("Collect")]
-        public bool UseExitCode { get; set; }
-        [ScriptAlias("Debug")]
-        [Description("Captures the PowerShell Write-Debug stream into Otter's execution debug log.")]
-        [Category("Logging")]
-        public bool DebugLogging { get; set; }
-        [ScriptAlias("Verbose")]
-        [Description("Captures the PowerShell Write-Verbose stream into Otter's execution debug log.")]
-        [Category("Logging")]
-        public bool VerboseLogging { get; set; }
-        [ScriptAlias("CollectScriptParams")]
-        [DisplayName("Collection script parameters")]
-        [Description("Map containing named arguments to pass to the PowerShell collect script.")]
-        [FieldEditMode(FieldEditMode.Multiline)]
-        [Category("Collect")]
-        public IReadOnlyDictionary<string, RuntimeValue> CollectScriptParams { get; set; }
-
-        protected override ExtendedRichDescription GetDescription(IOperationConfiguration config)
-        {
-            if (!string.IsNullOrWhiteSpace(config[nameof(this.CollectScript)]))
-            {
-                return new ExtendedRichDescription(
-                    new RichDescription(
-                        "Verify ",
-                        new Hilite(config[nameof(this.CollectScript)])
-                    ),
-                    new RichDescription("using PowerShell")
-                );
-            }
-            else if (!string.IsNullOrWhiteSpace(config[nameof(this.CollectScriptAsset)]))
-            {
-                return new ExtendedRichDescription(
-                    new RichDescription(
-                        "Verify ",
-                        new Hilite(config[nameof(this.CollectScriptAsset)])
-                    ),
-                    new RichDescription("using PowerShell script asset")
-                );
-            }
-            else
-            {
-                return new ExtendedRichDescription(
-                    new RichDescription(
-                        "Verify ",
-                        new Hilite("PowerShell")
-                    )
-                );
-            }
-        }
+        public RuntimeValue DefaultArgument { get; set; }
+        public IReadOnlyDictionary<string, RuntimeValue> NamedArguments { get; set; }
+        public IDictionary<string, RuntimeValue> OutArguments { get; set; }
 
         public override async Task<PersistedConfiguration> CollectAsync(IOperationCollectionContext context)
         {
-            if (!this.ValidateConfiguration())
+            var scriptName = this.DefaultArgument.AsString();
+            if (string.IsNullOrWhiteSpace(scriptName))
+            {
+                this.LogError("Bad or missing script name.");
                 return null;
+            }
 
-            var result = await PSUtil.ExecuteScriptAsync(
+            if (context.Simulation)
+            {
+                this.LogInformation("Executing PowerShell Script...");
+                return null;
+            }
+
+            var result = await PSUtil.ExecuteScriptAssetAsync(
                 logger: this,
                 context: context,
-                scriptNameOrContent: AH.CoalesceString(this.CollectScriptAsset, this.CollectScript),
-                scriptIsAsset: !string.IsNullOrWhiteSpace(this.CollectScriptAsset),
-                arguments: this.CollectScriptParams ?? new Dictionary<string, RuntimeValue>(),
-                outArguments: new Dictionary<string, RuntimeValue>(),
-                collectOutput: !this.UseExitCode,
-                progressUpdateHandler: (s, e) => Interlocked.Exchange(ref this.currentProgress, e)
+                fullScriptName: scriptName,
+                arguments: this.NamedArguments,
+                outArguments: this.OutArguments,
+                collectOutput: false,
+                useAhDirectives: true,
+                progressUpdateHandler: (s, e) => this.currentProgress = e,
+                executionMode: "Collect"
             );
 
             return new KeyValueConfiguration
             {
-                Key = this.ConfigurationKey,
-                Value = this.UseExitCode ? result.ExitCode?.ToString() : string.Join(", ", result.Output)
+                Key = result.ConfigKey,
+                Value = result.ConfigValue.ToString()
             };
         }
-
-        public override PersistedConfiguration GetConfigurationTemplate()
-        {
-            return new KeyValueConfiguration
-            {
-                Key = this.ConfigurationKey,
-                Value = this.ExpectedValue
-            };
-        }
-
         public override OperationProgress GetProgress()
         {
             var p = this.currentProgress;
             return new OperationProgress(p?.PercentComplete, p?.Activity);
         }
 
-        private bool ValidateConfiguration()
+        protected override ExtendedRichDescription GetDescription(IOperationConfiguration config)
         {
-            bool valid = true;
+            if (string.IsNullOrWhiteSpace(config.DefaultArgument))
+                return new ExtendedRichDescription(new RichDescription("PSVerify {error parsing statement}"));
 
-            if (string.IsNullOrWhiteSpace(this.CollectScript) && string.IsNullOrWhiteSpace(this.CollectScriptAsset))
+            var defaultArg = config.DefaultArgument;
+            var longDesc = new RichDescription();
+
+            bool longDescInclused = false;
+            var scriptName = LooselyQualifiedName.TryParse(defaultArg);
+            if (scriptName != null)
             {
-                this.LogError("Collect script missing. Specify a value for either \"Collect\" or \"CollectScript\".");
-                valid = false;
+                var info = PowerShellScriptInfo.TryLoad(scriptName);
+                if (!string.IsNullOrEmpty(info?.Description))
+                {
+                    longDesc.AppendContent(info.Description);
+                    longDescInclused = true;
+                }
+
+                var listParams = new List<string>();
+                foreach (var prop in config.NamedArguments)
+                    listParams.Add($"{prop.Key}: {prop.Value}");
+
+                foreach (var prop in config.OutArguments)
+                    listParams.Add($"{prop.Key} => {prop.Value}");
+
+                if (listParams.Count > 0)
+                {
+                    if (longDescInclused)
+                        longDesc.AppendContent(" - ");
+
+                    longDesc.AppendContent(new ListHilite(listParams));
+                    longDescInclused = true;
+                }
             }
 
-            if (!string.IsNullOrWhiteSpace(this.CollectScript) && !string.IsNullOrWhiteSpace(this.CollectScriptAsset))
-            {
-                this.LogError("Values are specified for both \"Collect\" and \"CollectScript\". Specify only one of each.");
-                valid = false;
-            }
+            if (!longDescInclused)
+                longDesc.AppendContent("with no parameters");
 
-            return valid;
+            return new ExtendedRichDescription(
+                new RichDescription("PSVerify ", new Hilite(defaultArg)),
+                longDesc
+            );
         }
     }
 }
