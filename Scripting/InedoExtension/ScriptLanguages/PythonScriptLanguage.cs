e49c1f06bd4c0760bdab6c2e5e31fc89aa59b65d
Richard Hessinger <rhessinger@inedo.com>
2022-02-07 17:31:00 -0500

    Tweaks to include icon and intellisense

--- Scripting/InedoExtension/ScriptLanguages/PythonScriptLanguage.cs
+++ Scripting/InedoExtension/ScriptLanguages/PythonScriptLanguage.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
 using Inedo.Documentation;
+using Inedo.Extensibility;
 using Inedo.Extensibility.RaftRepositories;
 using Inedo.Extensibility.ScriptLanguages;
 using Inedo.Extensions.Scripting.Operations.Python;
@@ -12,7 +15,9 @@ namespace Inedo.Extensions.Scripting.ScriptLanguages
     public sealed class PythonScriptLanguage : ScriptLanguageInfo
     {
         public override string LanguageName => "Python";
+        public override string SyntaxName => "python";
         public override string FileExtension => ".py";
+        public override FileResource Icon => new EmbeddedFileResource("script-python", "image/svg+xml", 2118);
         public override ScriptParameterUsage ParameterUsage => ScriptParameterUsage.InputVariable | ScriptParameterUsage.OutputVariable | ScriptParameterUsage.EnvironmentVariable | ScriptParameterUsage.Arguments;
         public override Type CallOperationType => typeof(PYCallOperation);
         public override Type EnsureOperationType => typeof(PYEnsureOperation);
@@ -24,5 +29,29 @@ namespace Inedo.Extensions.Scripting.ScriptLanguages
             using var reader = script.OpenTextReader();
             return ScriptParser.Parse<PythonScriptParser>(reader);
         }
+    }
+
+    public sealed class EmbeddedFileResource : FileResource
+    {
+        private string ResourceName => resourceName.Value;
+
+        private Lazy<Assembly> assembly;
+        private Lazy<string> resourceName;
+
+        public EmbeddedFileResource(string resourceName, string contentType, long size)
+        {
+            this.assembly = new Lazy<Assembly>(() => this.GetType().Assembly);
+            this.resourceName = new Lazy<string>(() => assembly.Value.GetManifestResourceNames()
+               .FirstOrDefault(n => n.EndsWith("." + resourceName, StringComparison.OrdinalIgnoreCase))
+            );
+            this.ContentType = contentType;
+            this.Size = size;
+        }
+
+        public override long Size { get; }
+
+        public override string ContentType { get; }
+
+        public override System.IO.Stream OpenRead() => assembly.Value.GetManifestResourceStream(this.ResourceName);
     }
 }
