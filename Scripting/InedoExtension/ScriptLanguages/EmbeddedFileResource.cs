e6e17067342744c8a9785768f71170140a799b94
Greg Divis, Inedo <gdivis@inedo.com>
2022-02-08 10:13:56 -0500

    tweaks to file resource

---
+++ Scripting/InedoExtension/ScriptLanguages/EmbeddedFileResource.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using Inedo.Extensibility;
+
+namespace Inedo.Extensions.Scripting.ScriptLanguages
+{
+    public sealed class EmbeddedFileResource : FileResource
+    {
+        private readonly string resourceName;
+        private readonly Lazy<long> size;
+        private readonly Lazy<DateTimeOffset> modified;
+
+        public EmbeddedFileResource(string resourceName, string contentType)
+        {
+            this.resourceName = $"{typeof(PythonScriptLanguage).Namespace}.{resourceName}";
+            this.size = new Lazy<long>(this.GetSize);
+            this.modified = new Lazy<DateTimeOffset>(this.GetModified);
+            this.ContentType = contentType;
+        }
+
+        public override string ContentType { get; }
+        public override long Size => this.size.Value;
+        public override DateTimeOffset? Modified => this.modified.Value;
+
+        public override Stream OpenRead() => typeof(PythonScriptLanguage).Assembly.GetManifestResourceStream(this.resourceName);
+
+        private long GetSize()
+        {
+            using var s = this.OpenRead();
+            return s.Length;
+        }
+        private DateTimeOffset GetModified() => File.GetLastWriteTimeUtc(typeof(PythonScriptLanguage).Assembly.Location);
+    }
+}
--- Scripting/InedoExtension/ScriptLanguages/PythonScriptLanguage.cs
+++ Scripting/InedoExtension/ScriptLanguages/PythonScriptLanguage.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.ComponentModel;
-using System.IO;
 using Inedo.Documentation;
 using Inedo.Extensibility;
 using Inedo.Extensibility.RaftRepositories;
@@ -16,7 +15,7 @@ namespace Inedo.Extensions.Scripting.ScriptLanguages
         public override string LanguageName => "Python";
         public override string SyntaxName => "python";
         public override string FileExtension => ".py";
-        public override FileResource Icon => new EmbeddedFileResource("script-python.svg", "image/svg+xml");
+        public override FileResource Icon { get; } = new EmbeddedFileResource("script-python.svg", "image/svg+xml");
         public override ScriptParameterUsage ParameterUsage => ScriptParameterUsage.InputVariable | ScriptParameterUsage.OutputVariable | ScriptParameterUsage.EnvironmentVariable | ScriptParameterUsage.Arguments;
         public override Type CallOperationType => typeof(PYCallOperation);
         public override Type EnsureOperationType => typeof(PYEnsureOperation);
@@ -28,33 +27,5 @@ namespace Inedo.Extensions.Scripting.ScriptLanguages
             using var reader = script.OpenTextReader();
             return ScriptParser.Parse<PythonScriptParser>(reader);
         }
-    }
-
-    public sealed class EmbeddedFileResource : FileResource
-    {
-        private readonly string resourceName;
-        private readonly Lazy<long> size;
-        private readonly Lazy<DateTimeOffset> modified;
-
-        public EmbeddedFileResource(string resourceName, string contentType)
-        {
-            this.resourceName = $"{typeof(PythonScriptLanguage).Namespace}.{resourceName}";
-            this.size = new Lazy<long>(this.GetSize);
-            this.modified = new Lazy<DateTimeOffset>(this.GetModified);
-            this.ContentType = contentType;
-        }
-
-        public override string ContentType { get; }
-        public override long Size => this.size.Value;
-        public override DateTimeOffset? Modified => this.modified.Value;
-
-        public override Stream OpenRead() => typeof(PythonScriptLanguage).Assembly.GetManifestResourceStream(this.resourceName);
-
-        private long GetSize()
-        {
-            using var s = this.OpenRead();
-            return s.Length;
-        }
-        private DateTimeOffset GetModified() => File.GetLastWriteTimeUtc(typeof(PythonScriptLanguage).Assembly.Location);
     }
 }
