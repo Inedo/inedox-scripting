1be08f53fb016c90354aac9e1c8e3ce1458a2716
Greg Divis <gdivis@inedo.com>
2026-03-11 16:40:10 -0400

    Update for sdk 4.0

--- Scripting/InedoExtension/SuggestionProviders/ScriptNameSuggestionProvider.cs
+++ Scripting/InedoExtension/SuggestionProviders/ScriptNameSuggestionProvider.cs
@@ -1,27 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
+using System.Threading;
 using Inedo.Extensibility;
 using Inedo.Extensibility.RaftRepositories;
 using Inedo.Web;
 
-namespace Inedo.Extensions.Scripting.SuggestionProviders
-{
-    internal sealed class ScriptNameSuggestionProvider : ISuggestionProvider
-    {
-        public Task<IEnumerable<string>> GetSuggestionsAsync(IComponentConfiguration config)
-        {
-            return Task.FromResult(getItems());
+namespace Inedo.Extensions.Scripting.SuggestionProviders;
 
-            IEnumerable<string> getItems()
-            {
-                return SDK.GetRaftItems(RaftItemType.Script, config.EditorContext)
-                    .Where(i => i.Name.EndsWith(".sh", StringComparison.OrdinalIgnoreCase))
-                    .OrderBy(i => i.Name)
-                    .Select(i => i.Name)
-                    .ToList();
-            }
+internal sealed class ScriptNameSuggestionProvider : ISuggestionProvider
+{
+    public IAsyncEnumerable<string> GetSuggestionsAsync(IComponentConfiguration config, CancellationToken cancellationToken)
+    {
+        return getItems().ToAsyncEnumerable();
+
+        IEnumerable<string> getItems()
+        {
+            return SDK.GetRaftItems(RaftItemType.Script, config.EditorContext)
+                .Where(i => i.Name.EndsWith(".sh", StringComparison.OrdinalIgnoreCase))
+                .OrderBy(i => i.Name)
+                .Select(i => i.Name);
         }
     }
 }
